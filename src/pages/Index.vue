4aa1cef94e7efe2e5533229299e07b12f7949227
Tommy Cox <tommy.cox@protonmail.com>
2020-02-20 10:14:03 -0700

    Slider proof of concept

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,8 +1,8 @@
 <template>
   <q-page class="flex flex-center">
-    <div class="q-pa-md" style="max-width: 350px">
+    <div class="q-pa-md">
       <q-list>
-        <q-item style="width: 400px">
+        <q-item>
           <q-item-section avatar>
             <q-avatar>
               <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
@@ -12,167 +12,129 @@
             <q-item-label>My Contribution</q-item-label>
           </q-item-section>
 
-          <q-item-section align="right">
-            140 DAI
-          </q-item-section>
+          <q-item-section align="right"> {{ total }} DAI </q-item-section>
         </q-item>
 
-        <q-item style="width: 400px">
+        <q-item class="row">
           <q-item-section avatar>
             <q-avatar>
               <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
             </q-avatar>
           </q-item-section>
-          <q-item-section>
+          <q-item-section class="col-3">
             <q-item-label>Burner Wallet 2 & Burner Factory</q-item-label>
             <q-item-label caption>0x32fF...32Fv</q-item-label>
           </q-item-section>
 
-          <q-item-section side style="width: 150px">
-            <q-slider v-model="project1" />
+          <q-item-section class="col-6">
+            <q-slider v-model="sliders[0]" @input="input" />
           </q-item-section>
 
-          <q-item-section align="right">
-            <q-item-label>20 DAI</q-item-label>
-            <q-item-label caption>{{ project1 }}%</q-item-label>
+          <q-item-section class="col-3" align="right">
+            <q-item-label>
+              {{ Math.round(total * sliders[0] * 0.01) }} DAI
+            </q-item-label>
+            <q-item-label caption>{{ Math.round(sliders[0]) }}%</q-item-label>
           </q-item-section>
         </q-item>
 
         <q-separator spaced inset />
 
-        <q-item style="width: 400px">
+        <q-item>
           <q-item-section avatar>
             <q-avatar>
               <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
             </q-avatar>
           </q-item-section>
-          <q-item-section>
+          <q-item-section class="col-3">
             <q-item-label>Tornado.cash</q-item-label>
             <q-item-label caption>0x32fF...32Fv</q-item-label>
           </q-item-section>
 
-          <q-item-section side style="width: 150px">
-            <q-slider v-model="project2" />
+          <q-item-section class="col-6">
+            <q-slider v-model="sliders[1]" @input="input" />
           </q-item-section>
 
-          <q-item-section align="right">
-            <q-item-label>20 DAI</q-item-label>
-            <q-item-label caption>{{ project2 }}%</q-item-label>
+          <q-item-section class="col-3" align="right">
+            <q-item-label>
+              {{ Math.round(total * sliders[1] * 0.01) }} DAI
+            </q-item-label>
+            <q-item-label caption>{{ Math.round(sliders[1]) }}%</q-item-label>
           </q-item-section>
         </q-item>
 
         <q-separator spaced inset />
 
-        <q-item style="width: 400px">
+        <q-item>
           <q-item-section avatar>
             <q-avatar>
               <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
             </q-avatar>
           </q-item-section>
-          <q-item-section>
+          <q-item-section class="col-3">
             <q-item-label>Validity</q-item-label>
             <q-item-label caption>0x32fF...32Fv</q-item-label>
           </q-item-section>
 
-          <q-item-section side style="width: 150px">
-            <q-slider v-model="project3" />
+          <q-item-section class="col-6">
+            <q-slider v-model="sliders[2]" @input="input" />
           </q-item-section>
 
-          <q-item-section align="right">
-            <q-item-label>20 DAI</q-item-label>
-            <q-item-label caption>{{ project3 }}%</q-item-label>
+          <q-item-section class="col-3" align="right">
+            <q-item-label>
+              {{ Math.round(total * sliders[2] * 0.01) }} DAI
+            </q-item-label>
+            <q-item-label caption>{{ Math.round(sliders[2]) }}%</q-item-label>
           </q-item-section>
         </q-item>
 
         <q-separator spaced inset />
 
-        <q-item style="width: 400px">
+        <q-item>
           <q-item-section avatar>
             <q-avatar>
               <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
             </q-avatar>
           </q-item-section>
-          <q-item-section>
+          <q-item-section class="col-3">
             <q-item-label>DAppNode</q-item-label>
             <q-item-label caption>0x32fF...32Fv</q-item-label>
           </q-item-section>
 
-          <q-item-section side style="width: 150px">
-            <q-slider v-model="project4" />
+          <q-item-section class="col-6">
+            <q-slider v-model="sliders[3]" @input="input" />
           </q-item-section>
 
-          <q-item-section align="right">
-            <q-item-label>20 DAI</q-item-label>
-            <q-item-label caption>{{ project4 }}%</q-item-label>
+          <q-item-section class="col-3" align="right">
+            <q-item-label>
+              {{ Math.round(total * sliders[3] * 0.01) }} DAI
+            </q-item-label>
+            <q-item-label caption>{{ Math.round(sliders[3]) }}%</q-item-label>
           </q-item-section>
         </q-item>
 
         <q-separator spaced inset />
 
-        <q-item style="width: 400px">
+        <q-item>
           <q-item-section avatar>
             <q-avatar>
               <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
             </q-avatar>
           </q-item-section>
-          <q-item-section>
+          <q-item-section class="col-3">
             <q-item-label>Sablier</q-item-label>
             <q-item-label caption>0x32fF...32Fv</q-item-label>
           </q-item-section>
 
-          <q-item-section side style="width: 150px">
-            <q-slider v-model="project5" />
+          <q-item-section class="col-6">
+            <q-slider v-model="sliders[4]" @input="input" />
           </q-item-section>
 
-          <q-item-section align="right">
-            <q-item-label>20 DAI</q-item-label>
-            <q-item-label caption>{{ project5 }}%</q-item-label>
-          </q-item-section>
-        </q-item>
-
-        <q-separator spaced inset />
-
-        <q-item style="width: 400px">
-          <q-item-section avatar>
-            <q-avatar>
-              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-            </q-avatar>
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Marketing DAO</q-item-label>
-            <q-item-label caption>0x32fF...32Fv</q-item-label>
-          </q-item-section>
-
-          <q-item-section side style="width: 150px">
-            <q-slider v-model="project6" />
-          </q-item-section>
-
-          <q-item-section align="right">
-            <q-item-label>20 DAI</q-item-label>
-            <q-item-label caption>{{ project6 }}%</q-item-label>
-          </q-item-section>
-        </q-item>
-
-        <q-separator spaced inset />
-
-        <q-item style="width: 400px">
-          <q-item-section avatar>
-            <q-avatar>
-              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-            </q-avatar>
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Recipient 7</q-item-label>
-            <q-item-label caption>0x32fF...32Fv</q-item-label>
-          </q-item-section>
-
-          <q-item-section side style="width: 150px">
-            <q-slider v-model="project7" />
-          </q-item-section>
-
-          <q-item-section align="right">
-            <q-item-label>20 DAI</q-item-label>
-            <q-item-label caption>{{ project7 }}%</q-item-label>
+          <q-item-section class="col-3" align="right">
+            <q-item-label>
+              {{ Math.round(total * sliders[4] * 0.01) }} DAI
+            </q-item-label>
+            <q-item-label caption>{{ Math.round(sliders[4]) }}%</q-item-label>
           </q-item-section>
         </q-item>
       </q-list>
@@ -188,14 +150,43 @@ export default {
   name: "PageIndex",
   data() {
     return {
-      project1: 20,
-      project2: 20,
-      project3: 20,
-      project4: 20,
-      project5: 20,
-      project6: 20,
-      project7: 20
+      sliders: [20, 20, 20, 20, 20],
+      total: 140
     };
+  },
+  methods: {
+    input(n) {
+      const sum = this.sliders.reduce((sum, val) => sum + val, 0);
+      const diff = sum - 100;
+      let remainder = 0;
+      console.log(diff);
+      for (let i in this.sliders) {
+        if (i != n) {
+          //don't modify the slider which is being dragged
+          let val = this.sliders[i] - diff / (this.sliders.length - 1);
+          if (val < 0) {
+            remainder += val;
+            val = 0;
+          }
+          this.$set(this.sliders, i, val);
+        }
+      }
+      if (remainder) {
+        const filteredLength = this.sliders.filter(
+          (val, key) => val > 0 && key != n
+        ).length;
+        for (let i in this.sliders) {
+          if (i != n && this.sliders[i] > 0) {
+            this.$set(
+              this.sliders,
+              i,
+              this.sliders[i] + remainder / filteredLength
+            );
+          }
+        }
+      }
+      this.$emit("input", this.sliders);
+    }
   }
 };
 </script>
